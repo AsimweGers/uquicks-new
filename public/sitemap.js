cf0029a0575ffaad56da010befdd07c75fcb09f3
AsimweGers <asimwegers96@gmail.com>
2022-12-04 22:20:08 +0300

    sitemap

--- public/sitemap.js
+++ public/sitemap.js
@@ -4,57 +4,92 @@ const { Readable } = required("stream");
 export default async (req, res) => {
   const links = [
     {
-      url: "https://sdamusic.uquicks.com/post/nashimwe-by-ambassadors-of-christ-choir-rwanda-car-insurance",
+      url: "https://uquicks.com/AboutUs",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/sifa-by-golden-gate-choir-uganda-get-life-insurance-in-jesus",
+      url: "https://www.uquicks.com/ContactUs",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/nashimwe-by-ambassadors-of-christ-choir-rwanda",
+      url: "https://www.uquicks.com/Terms-&-Conditions",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/ekalenda-by-licoasa-sda-choir-kasese-classes-of-degree",
+      url: "https://www.uquicks.com/Privacy-Policy",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/ani-alimponya-by-emerald-gospel-singers-kagote-fort-portal-uganda-god-is-our-only-attorney",
+      url: "https://www.uquicks.com/post/ntume-ani-by-golden-gate-choir",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/best-sda-nonstop-uganda-2022-sda-mission-story-sda-lesson",
+      url: "https://www.uquicks.com/post/maria-by-ambassadors-of-christ",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/sweet-by-and-by-with-lyrics-by-bird-youmans",
+      url: "https://www.uquicks.com/post/mercy-chinwo-obinasom",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/nitashinda-by-sarah-uwera",
+      url: "https://www.uquicks.com/post/ada-ehi-jesus-you-are-able",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/when-we-all-get-to-heaven-with-lyrics-classic-gospel-song-hymn-bird-youmans",
+      url: "https://www.uquicks.com/post/top-200-ugandan-gospel-songs-of-all-time-luganda-worship-gospel-nonstop-2022-dj-mixing-software",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/mwana-wanjye-by-sarah-uwera-of-ambassadors-of-christ-rwanda",
+      url: "https://www.uquicks.com/post/mukama-osanidde-by-phoebe-ashaba",
       changefreq: "daily",
       priority: 0.3,
     },
     {
-      url: "https://sdamusic.uquicks.com/post/baba-nisaidie-by-clovice-mugume-sda-music-2022",
+      url: "https://www.uquicks.com/post/tukutendereza-yesu-jehovah-shalom-acapella",
+      changefreq: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "https://www.uquicks.com/post/joy-to-the-world-by-jehovah-shalom-acapella-sdamissionug",
+      changefreq: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "https://www.uquicks.com/post/are-you-single-and-searching-by-jehovah-shalom-acapella",
+      changefreq: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "https://www.uquicks.com/post/wano-by-ntaate",
+      changefreq: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "https://www.uquicks.com/post/have-a-little-talk-with-jesus-by-jehovah-shalom-acapella",
+      changefreq: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "https://www.uquicks.com/post/awo-by-ntaate-sda-mission-ug",
+      changefreq: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "https://www.uquicks.com/post/weapon-of-victory-by-ntaate-sdamissionug-com",
+      changefreq: "daily",
+      priority: 0.3,
+    },
+    {
+      url: "https://www.uquicks.com/post/ruhanga-akantorana-by-ntaate-insurance-in-jesus",
       changefreq: "daily",
       priority: 0.3,
     },
