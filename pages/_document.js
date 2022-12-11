435e4e0bf72bf220a2b6515ac640e0c7ea8e9c7e
AsimweGers <asimwegers96@gmail.com>
2022-12-11 05:04:56 +0300

    seo

--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-
-{/* <!-- Ezoic Code --> */}
-	<script>var ezoicId = 413266;</script>
-	<script type="text/javascript" src="//go.ezoic.net/ezoic/ezoic.js"></script>
-{/* <!-- Ezoic Code --> */}
-
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9981376163607142"
